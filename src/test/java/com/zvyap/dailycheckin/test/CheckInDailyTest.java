dead8bcbaf76940737ebeec17204644627c677bf
knightroo <dk.prajaa@gmail.com>
2024-02-25 23:03:05 +0700

    Update CheckInDailyTest.java

--- src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
+++ src/test/java/com/zvyap/dailycheckin/test/CheckInDailyTest.java
@@ -24,8 +24,23 @@ public class CheckInDailyTest {
 
                 CheckInAction.builder()
                         .token(HoyoToken.of(System.getenv("USER_1_ID"), System.getenv("USER_1_TOKEN")))
-                        .game(GameType.GENSHIN_IMPACT)
                         .game(GameType.HONKAI_STAR_RAIL)
+                        .build(),
+            
+                CheckInAction.builder()
+                        .token(HoyoToken.of(System.getenv("USER_2_ID"), System.getenv("USER_2_TOKEN")))
+                        .game(GameType.HONKAI_STAR_RAIL)
+                        .build(),
+            
+                CheckInAction.builder()
+                        .token(HoyoToken.of(System.getenv("USER_3_ID"), System.getenv("USER_3_TOKEN")))
+                        .game(GameType.HONKAI_STAR_RAIL)
+                        .build(),
+
+                CheckInAction.builder()
+                        .token(HoyoToken.of(System.getenv("USER_4_ID"), System.getenv("USER_4_TOKEN")))
+                        .game(GameType.GENSHIN_IMPACT)
+                        .game(GameType.HONKAI_IMPACT_3RD)
                         .build()
 
 
